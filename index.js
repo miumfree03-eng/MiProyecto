907f1ceeacfefe0b37810c6320b2b56f4f07b2cb
Anton <miumfree03@gmail.com>
2026-03-18 18:17:55 -0600

    Definiendo dbConfig correctamente

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    port: parseInt(process.env.DB_PORT) || 3306, // Usar parseInt ayuda a evitar errores
+    connectTimeout: 10000 // Le da 10 segundos para intentar conectar
 };
 
 app.get('/', (req, res) => {
